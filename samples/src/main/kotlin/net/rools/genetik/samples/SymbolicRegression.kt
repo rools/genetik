07382b964e08a1f50bf97365413342c33f2e5433
Robert Olsson <rools1@gmail.com>
2023-08-23 21:12:40 +0200

    Simplify symbolic regression sample

--- samples/src/main/kotlin/net/rools/genetik/samples/SymbolicRegression.kt
+++ samples/src/main/kotlin/net/rools/genetik/samples/SymbolicRegression.kt
@@ -2,7 +2,6 @@ package net.rools.genetik.samples
 
 import net.rools.genetik.*
 import net.rools.genetik.cache.NoFitnessCache
-import net.rools.genetik.geneticoperation.EliteOperation
 import net.rools.genetik.geneticoperation.ReproductionOperation
 import net.rools.genetik.geneticoperation.SubtreeCrossoverOperation
 import net.rools.genetik.geneticoperation.SubtreeMutationOperation
@@ -13,31 +12,28 @@ import kotlin.math.pow
 import kotlin.random.Random
 
 fun main() {
-    val nodeTypes = listOf<NodeType<*, in VariableHolder>>(
-        function("+") { a: Number, b: Double -> a.toFloat() + b },
-        function("-") { a: Double, b: Double -> a - b },
-        function("^") { a: Double, b: Double -> a.pow(b) },
-        function("*") { a: Double, b: Double -> a * b },
-        function("/") { a: Double, b: Double -> a / b },
+    val nodeTypes = listOf<NodeType<*, in Int>>(
+        function("+") { a: Int, b: Int -> a + b },
+        function("-") { a: Int, b: Int -> a - b },
+        function("*") { a: Int, b: Int -> a * b },
 
-        variable("x") { v: VariableHolder -> v.x },
+        variable("x") { x: Int -> x },
 
-        ephemeralConstant{ random -> random.nextInt(-10, 10).toDouble() }
+        ephemeralConstant { random -> random.nextInt(1, 10) }
     )
 
     val params = GeneticParameters(
         nodeTypes = nodeTypes,
-        rootType = Double::class,
-        fitnessEvaluator = SymbolicRegressionEvaluator(),
+        rootType = Int::class,
+        fitnessEvaluator = MeanSquaredErrorEvaluator(),
         populationInitializer = RampedHalfAndHalfInitializer(),
         geneticOperations = listOf(
-            EliteOperation(count = 10),
             SubtreeCrossoverOperation(probability = 0.8),
             SubtreeMutationOperation(probability = 0.25),
             ReproductionOperation(probability = 1.0)
         ),
         terminationCriterion = GenerationTerminationCriterion(100),
-        random = Random(1),
+        random = Random(seed = 1),
         populationSize = 100_000,
         maxNodeDepth = 4,
         optimizationType = OptimizationType.MINIMIZE,
@@ -46,43 +42,24 @@ fun main() {
 
     val runner = GeneticRunner(
         params = params,
-        evolutionListeners = listOf(
-            LoggingEvolutionListener()
-        ),
+        evolutionListeners = listOf(LoggingEvolutionListener()),
         fitnessCache = NoFitnessCache()
     )
 
     runner.evolve()
 }
 
-private class SymbolicRegressionEvaluator : FitnessEvaluator<Double, VariableHolder> {
-    private val values = mutableListOf<Pair<Double, Double>>()
+private class MeanSquaredErrorEvaluator : FitnessEvaluator<Int, Int> {
+    private fun targetExpression(x: Int) = (7 * x - 3) * x + 5
 
-    init {
-        fun expression(x: Double) = x.pow(3) / 5 + 2 * x - 7
-
-        var x = -5.0
-        while (x < 5.0) {
-            values += x to expression(x)
-            x += 0.1
-        }
+    private val dataset: List<Pair<Int, Int>> = (-10..10).map { x ->
+        x to targetExpression(x)
     }
 
-    override fun evaluate(individual: Individual<out Double, VariableHolder>): Double {
-        val variables = VariableHolder(0.0)
-
-        val mse = values.sumOf { (x, targetY) ->
-            variables.x = x
-            val y = individual.rootNode.evaluate(variables)
-            (targetY - y).pow(2)
-        }
-
-        if (mse.isNaN()) return Double.POSITIVE_INFINITY
-
-        return mse
+    override fun evaluate(individual: Individual<out Int, Int>): Double {
+        return dataset.sumOf { (x, expectedY) ->
+            val evaluatedY = individual.rootNode.evaluate(x)
+            (expectedY - evaluatedY).toDouble().pow(2)
+        } / dataset.size
     }
 }
-
-private data class VariableHolder(
-    var x: Double
-)
